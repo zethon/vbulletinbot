7df448912e26b8a4e4c2f4bc3f469bdb41f46fe9
aclaure <aclaure@95dce702-dc8c-11de-b878-c5ec750a8c44>
2010-07-19 20:02:54 +0000

    Changed VBEntity to IVBEntity

--- server/vbotserver/Post.cs
+++ server/vbotserver/Post.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 
 namespace VBulletinBot.VBotService
 {
-    public partial class Post : VBEntity
+    public partial class Post : IVBEntity
     {
         public int DatabaseID
         {
@@ -19,28 +19,6 @@ namespace VBulletinBot.VBotService
             {
                 PostID = value;
             }
-        }
-
-        public string GetFriendlyDate()
-        {
-            string strRet = string.Empty;
-            DateTime dt = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).ToLocalTime().AddSeconds(DateLine);
-            string strDate = dt.ToShortDateString();
-            string strTime = dt.ToShortTimeString();
-
-            if (dt.Date == DateTime.Now.Date)
-            {
-                strDate = "Today";
-            }
-            else if (dt.Date == DateTime.Now.Date.AddDays(-1))
-            {
-                strDate = "Yesterday";
-            }
-
-            strRet = strDate + " " + strTime;
-            
-
-            return strRet;
         }
 
         public string GetShortPostText()
--- server/vbotserver/Thread.cs
+++ server/vbotserver/Thread.cs
@@ -6,7 +6,7 @@ using System.Text.RegularExpressions;
 
 namespace VBulletinBot.VBotService
 {
-    public partial class Thread : VBEntity
+    public partial class Thread : IVBEntity
     {
         public int DatabaseID
         {
@@ -21,47 +21,11 @@ namespace VBulletinBot.VBotService
             }
         }
 
-        public string GetFriendlyDate()
-        {
-            return GetFriendlyDate(LastPost);
-        }
-
-        public string GetFriendlyDate(int iSeconds)
-        {
-            DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).ToLocalTime();
-            return GetFriendlyDate(epoch.AddSeconds(iSeconds));
-        }
-
-        public string GetFriendlyDate(DateTime theTime)
-        {
-            string strRet = string.Empty;
-
-            if (theTime != null)
-            {
-                string strDate = theTime.ToShortDateString();
-                string strTime = theTime.ToShortTimeString();
-
-                if (theTime.Date == DateTime.Now.Date)
-                {
-                    strDate = "Today";
-                }
-                else if (theTime.Date == DateTime.Now.Date.AddDays(-1))
-                {
-                    strDate = "Yesterday";
-                }
-
-                strTime = strTime.Replace(" ", string.Empty);
-                strRet = string.Format("{0} {1}", strDate, strTime,theTime.Date.ToString(),DateTime.Now.Date.ToString());
-            }
-
-            return strRet;
-        }
-
         public string GetTitle()
         {
             return string.Format("{0} - {1} created by {2}",
                              Regex.Replace(ThreadTitle, @"[\']", string.Empty),
-                             GetFriendlyDate(DateLine),
+                             DateLineText,
                              PostUsername);
         }
     }
